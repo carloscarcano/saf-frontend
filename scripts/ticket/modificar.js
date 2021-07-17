bf8c477caa661b78527dd975628b627272757293
carlos carcano <carlos.carcano@hotmail.com>
2021-07-17 14:50:33 -0500

    ajustes finales, inclusión de manual de usuario

--- scripts/ticket/modificar.js
+++ scripts/ticket/modificar.js
@@ -2,6 +2,9 @@ $(document).ready(function()
 {
     $('#lnkAceptar').click(lnkAceptar);
     $('#lnkCancelar').click(lnkCancelar);
+
+    // var hoy = new Date().toLocaleDateString().split('/');
+    // $('#txtFechaError').val(hoy[2] + '-' + (hoy[1].length == 1 ? '0' + hoy[1] : hoy[1]) + '-' + hoy[0]);
 });
 
 function lnkAceptar()
@@ -9,16 +12,21 @@ function lnkAceptar()
     event.preventDefault();
     $('#divMensajes').html('Espere...');
 
-    var idTicket = $('#txtIdTicket').val();
+    
     var descripcionError = $('#txtDescripcionError').val();
-    var fechaError = $('#txtFechaError').val();
-    var idEstadoTicket = $('#cmbEstadoTicket').val();
+    var fechaErrorAMD = $('#txtFechaError').val();
 
-    if (descripcionError == '' || fechaError == '')
+    if (descripcionError == '' || fechaErrorAMD == '')
     {
         $('#divMensajes').html('Todos los datos son requeridos.');
         return false;
     }
+
+    var fechaSplit = fechaErrorAMD.split('-');
+    var fechaError = fechaSplit[2] + '/' + fechaSplit[1] + '/' + fechaSplit[0];
+
+    var idTicket = $('#txtIdTicket').val();
+    var idEstadoTicket = $('#cmbEstadoTicket').val();
 
     $('#divBotones').hide();
 
--- scripts/ticket/nuevo.js
+++ scripts/ticket/nuevo.js
@@ -2,6 +2,9 @@ $(document).ready(function()
 {
     $('#lnkAceptar').click(lnkAceptar);
     $('#lnkCancelar').click(lnkCancelar);
+
+    var hoy = new Date().toLocaleDateString().split('/');
+    $('#txtFechaError').val(hoy[2] + '-' + (hoy[1].length == 1 ? '0' + hoy[1] : hoy[1]) + '-' + hoy[0]);
 });
 
 function lnkAceptar()
@@ -9,13 +12,16 @@ function lnkAceptar()
     event.preventDefault();
     $('#divMensajes').html('Espere...');
     var descripcionError = $('#txtDescripcionError').val();
-    var fechaError = $('#txtFechaError').val();
+    var fechaErrorAMD = $('#txtFechaError').val();
 
-    if (descripcionError == '' || fechaError == '')
+    if (descripcionError == '' || fechaErrorAMD == '')
     {
         $('#divMensajes').html('Todos los datos son requeridos.');
         return false;
     }
+
+    var fechaSplit = fechaErrorAMD.split('-');
+    var fechaError = fechaSplit[2] + '/' + fechaSplit[1] + '/' + fechaSplit[0];
 
     $('#divBotones').hide();
     
